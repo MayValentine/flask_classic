8b6f0a7d4a89c51378acb8ccbf293109eefc829f
May Jimenez <may_coloresjp@hotmail.com>
2022-09-28 00:19:40 +0200

    que jhdisvbl

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,5 +1,8 @@
 from registro_ig import app
+from flask import render_template
+from registro_ig.models import select_all
 
 @app.route("/")
 def index():
-    return "servidor rulando!!!"
+    registros = select_all()
+    return render_template("index.html", pageTitle="Todos", data=registros)
